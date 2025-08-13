6ff1b9f1a808df3e1dabe1adf0fbf57f3eae396e
Popa Iulian-Marian <iulianpopa167@gmail.com>
2025-08-13 17:41:18 +0300

    Using Input.GetAxis()

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,9 +2,7 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float xValue = 0f;
-    [SerializeField] float yValue = 0.01f;
-    [SerializeField] float zValue = 0f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,6 +13,9 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xValue = Input.GetAxis("Horizontal");
+        float yValue = 0f; // Assuming no vertical movement on the Y axis
+        float zValue = Input.GetAxis("Vertical");
         transform.Translate(xValue, yValue, zValue);
     }
 }
