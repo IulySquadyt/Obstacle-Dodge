dbc92eb695cabed0c47ab5d623e05d8951d00c3b
Popa Iulian-Marian <iulianpopa167@gmail.com>
2025-08-14 17:13:34 +0300

    Trigger Projectile

---
+++ Assets/Scripts/TriggerProjectile.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class TriggerProjectile : MonoBehaviour
+{
+    [SerializeField] GameObject[] projectiles;
+    private void OnTriggerEnter(Collider other)
+    {
+        foreach (GameObject projectile in projectiles)
+        {
+            if (other.gameObject.tag == "Player")
+            {
+                projectile.SetActive(true);
+            }
+        }
+        if (other.gameObject.tag == "Player")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
